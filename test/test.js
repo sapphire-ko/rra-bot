1f0123a595cbc77d5fa6fef5e57ec35ac8f4dc8f
sapphire <info@sapphire.sh>
2016-10-05 06:05:34 +0900

    fix test

--- test/test.js
+++ test/test.js
@@ -7,7 +7,8 @@ var app = new App();
 
 describe('@rra_bot', function() {
 	it('parse', function(done) {
-		this.timeout(10000);
+		this.timeout(20000);
+		this.retries(2);
 		app.parse('20161004', 1, function(res) {
 			assert.equal(res, true);
 			done();
