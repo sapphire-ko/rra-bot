64f8d423059a383675117ae5609a1a2fcdce0cdc
sapphire <info@sapphire.sh>
2021-04-18 19:26:27 +0900

    update params

--- src/helpers/sendRequest.ts
+++ src/helpers/sendRequest.ts
@@ -3,15 +3,33 @@ import fetch from 'node-fetch';
 import { Parameters } from '~/models';
 
 export const sendRequest = async (url: string, params?: Parameters): Promise<string> => {
+	const stringifyParams = (params: Parameters) => {
+		const values = [];
+		if(params.cpage) {
+			values.push(['cpage', params.cpage]);
+		}
+		values.push(...[
+			['category', ''],
+			['fromdate', params.fromdate],
+			['todate', params.todate],
+			['firm', ''],
+			['equip', ''],
+			['model_no', ''],
+			['app_no', ''],
+			['maker', ''],
+			['nation', ''],
+		]);
+		return values.map(x => x.join('=')).join('&');
+	};
 	const res = await fetch(url, {
 		method: 'POST',
 		headers: {
 			'content-type': 'application/x-www-form-urlencoded',
 			referer: url,
 		},
-		body: params ? `cpage=${params.cpage}&category=&fromdate=${params.fromdate}&todate=${params.todate}&firm=&equip=&model_no=&app_no=&maker=&nation=` : '',
+		body: params ? stringifyParams(params) : '',
 	});
-	if(!res.ok) {
+	if (!res.ok) {
 		throw new Error(res.statusText);
 	}
 	const body = await res.buffer();
